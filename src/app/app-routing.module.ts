39eff76aa9e6cd7cbb8ba5c5909b1baf2031a79d
Jesse Myers <jkmyersfamily@gmail.com>
2020-11-30 10:55:53 -0800

    added an image and some basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,33 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, Routes, RouterModule } from '@angular/router';
+import { GenHomeComponent } from './gen-home/gen-home.component';
+import { GenLoginComponent } from './gen-login/gen-login.component';
+import { GenSignupComponent } from './gen-signup/gen-signup.component';
+
 
 import { GenXerComponent } from './gen-xer/gen-xer.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'gen-xer', pathMatch: 'full'    
+    redirectTo: 'gen-xer',
+    pathMatch: 'full',
   },
 
   {
-    path: 'gen-xer', component: GenXerComponent
-  }
-
+    path: 'gen-xer',
+    component: GenXerComponent,
+  },
+  {path: 'home', component: GenHomeComponent},
+  {path: 'login', component: GenLoginComponent},
+  {path: 'signup', component: GenSignupComponent}
+ 
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules})
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/models/post.ts
@@ -0,0 +1,13 @@
+export class Post {
+// Title
+// Body
+// Author
+// Gen
+
+title: string;
+body: string;
+author: string;
+gen: string;
+
+}
+
